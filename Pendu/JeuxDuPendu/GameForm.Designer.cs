dd64368cb70f58ae9d0aec87a7bd894b7be04f85
Maxime BARATTE <114225556+e-Keno@users.noreply.github.com>
2022-12-15 09:40:12 +0100

    Add files via upload

---
+++ Pendu/JeuxDuPendu/GameForm.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace JeuxDuPendu
+{
+    partial class GameForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GameForm));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.bReset = new System.Windows.Forms.Button();
+            this.lCrypedWord = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.règlesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.créditsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 65);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(256, 256);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // bReset
+            // 
+            this.bReset.Location = new System.Drawing.Point(178, 379);
+            this.bReset.Name = "bReset";
+            this.bReset.Size = new System.Drawing.Size(90, 35);
+            this.bReset.TabIndex = 1;
+            this.bReset.Text = "Nouvelle partie";
+            this.bReset.UseVisualStyleBackColor = true;
+            this.bReset.Click += new System.EventHandler(this.bReset_Click);
+            this.bReset.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.bReset_KeyPress);
+            // 
+            // lCrypedWord
+            // 
+            this.lCrypedWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lCrypedWord.Location = new System.Drawing.Point(13, 324);
+            this.lCrypedWord.Name = "lCrypedWord";
+            this.lCrypedWord.Size = new System.Drawing.Size(255, 39);
+            this.lCrypedWord.TabIndex = 2;
+            this.lCrypedWord.Text = "--";
+            this.lCrypedWord.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lCrypedWord.Click += new System.EventHandler(this.lCrypedWord_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 401);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripDropDownButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(293, 25);
+            this.toolStrip1.TabIndex = 4;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.règlesToolStripMenuItem,
+            this.créditsToolStripMenuItem,
+            this.quitterToolStripMenuItem});
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(37, 22);
+            this.toolStripDropDownButton1.Text = "Jeu";
+            // 
+            // règlesToolStripMenuItem
+            // 
+            this.règlesToolStripMenuItem.Name = "règlesToolStripMenuItem";
+            this.règlesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.règlesToolStripMenuItem.Text = "Règles";
+            this.règlesToolStripMenuItem.Click += new System.EventHandler(this.règlesToolStripMenuItem_Click);
+            // 
+            // créditsToolStripMenuItem
+            // 
+            this.créditsToolStripMenuItem.Name = "créditsToolStripMenuItem";
+            this.créditsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.créditsToolStripMenuItem.Text = "Crédits";
+            this.créditsToolStripMenuItem.Click += new System.EventHandler(this.créditsToolStripMenuItem_Click);
+            // 
+            // quitterToolStripMenuItem
+            // 
+            this.quitterToolStripMenuItem.Name = "quitterToolStripMenuItem";
+            this.quitterToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.quitterToolStripMenuItem.Text = "Quitter";
+            this.quitterToolStripMenuItem.Click += new System.EventHandler(this.quitterToolStripMenuItem_Click);
+            // 
+            // GameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(293, 428);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lCrypedWord);
+            this.Controls.Add(this.bReset);
+            this.Controls.Add(this.panel1);
+            this.Name = "GameForm";
+            this.Text = "Jeux du pendu";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.GameForm_KeyPress);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button bReset;
+        private System.Windows.Forms.Label lCrypedWord;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem règlesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem créditsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitterToolStripMenuItem;
+    }
+}
+
---
+++ Pendu/JeuxDuPendu/GameForm.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using JeuxDuPendu.MyControls;
+
+namespace JeuxDuPendu
+{
+    public partial class GameForm : Form
+    {
+
+        Random r = new Random();
+
+        string[] mots = {"esiee", "poule", "azerty", "manger" }; 
+
+        string mot;
+        
+
+        // Initialisation de l'instance de la classe d'affichage du pendu.
+        HangmanViewer _HangmanViewer = new HangmanViewer();
+
+        /// <summary>
+        /// Constructeur du formulaire de jeux
+        /// </summary>
+        public GameForm()
+        {
+            InitializeComponent();
+            InitializeMyComponent();
+            StartNewGame();
+        }
+
+        /// <summary>
+        /// Initialisations des composants specifiques a l'application
+        /// </summary>
+        private void InitializeMyComponent()
+        {
+            // On positionne le controle d'affichage du pendu dans panel1 : 
+            panel1.Controls.Add(_HangmanViewer);
+			
+			// à la position 0,0
+            _HangmanViewer.Location = new Point(0, 0);
+			
+			// et de la même taille que panel1
+            _HangmanViewer.Size = panel1.Size;
+
+            //initialisation du mot
+        }
+
+        /// <summary>
+        /// Initialise une nouvelle partie
+        /// </summary>
+        public void StartNewGame()
+        {
+            // Methode de reinitialisation classe d'affichage du pendu.
+            _HangmanViewer.Reset();
+
+            mot = mots[r.Next(0, mots.Length)];
+
+            //Affichage du mot à trouver dans le label.
+            AfficheInit();
+        }
+
+        public void AfficheInit()
+        {
+            
+            StringBuilder myStringBuilder = new StringBuilder();
+            StringBuilder myStringBuilder2 = new StringBuilder();
+
+
+            for (int i = 0; i < mot.Length ; i++)
+            {
+                myStringBuilder.Append("_");
+                myStringBuilder2.Append("");
+            }
+            lCrypedWord.Text = myStringBuilder.ToString();
+
+            
+            label1.Text = myStringBuilder2.ToString();
+        }
+
+        
+        /// <summary>
+        /// Methode appelé lors de l'appui d'une touche du clavier, lorsque le focus est sur le bouton "Nouvelle partie"
+        /// </summary>
+        private void bReset_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            KeyPressed(e.KeyChar);
+        }
+
+        /// <summary>
+        /// Methode appelé lors de l'appui d'un touche du clavier, lorsque le focus est sur le formulaire
+        /// </summary>
+        private void GameForm_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            KeyPressed(e.KeyChar);
+
+        }
+
+        /// <summary>
+        /// Methode appelé lors de l'appui sur le bouton "Nouvelle partie"
+        /// </summary>
+        private void bReset_Click(object sender, EventArgs e)
+        {
+            StartNewGame();
+        }
+
+        private void KeyPressed(char letter)
+        {
+            StringBuilder myStringBuilder = new StringBuilder(lCrypedWord.Text);
+            StringBuilder myStringBuilder2 = new StringBuilder(label1.Text);
+            bool c = false;
+
+            for (int i=0;i< mot.Length; i++)
+            {
+                if (mot[i] == letter)
+                {
+                    myStringBuilder.Remove(i, 1);
+                    myStringBuilder.Insert(i, letter);
+                    c = true;
+                }
+
+              
+            }
+
+            if (c == false && !label1.Text.Contains(letter))
+            {
+                // On avance le pendu d'une etape
+                _HangmanViewer.MoveNextStep();
+                myStringBuilder2.Append(letter);
+            }
+
+            lCrypedWord.Text = myStringBuilder.ToString();
+            label1.Text = myStringBuilder2.ToString();
+
+
+            if (lCrypedWord.Text == mot)
+            {
+                MessageBox.Show("Vous avez gagné !");
+                StartNewGame();
+            }
+
+            // Si le pendu est complet, le joueur à perdu.
+            if (_HangmanViewer.IsGameOver)
+            {
+                MessageBox.Show("Vous avez perdu !");
+                StartNewGame();
+            }
+        }
+
+        
+
+        private void lCrypedWord_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void règlesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Le but du jeu est simple : deviner toute les lettres qui doivent composer un mot, éventuellement avec un nombre limité de tentatives et des thèmes fixés à l’avance.\n A chaque fois que le joueur devine une lettre, celle-ci est affichée. Dans le cas contraire, le dessin d’un pendu se met à apparaître…");
+        }
+
+        private void créditsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(" pendu crée par Paul Falloni, Benoit Gautier et Maxime Baratte");
+        }
+
+        private void quitterToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(" Merci d'avoir joué !");
+            Application.Exit();
+        }
+    }
+
+}
